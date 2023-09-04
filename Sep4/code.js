614d185ea127566ba496dbdfb2e126ee9e95f78f
Jordy <jordycorporan95@gmail.com>
2023-09-04 11:23:29 -0400

    Find nemo example

---
+++ Sep4/code.js
@@ -0,0 +1,10 @@
+const nemo = ['nemo']
+
+const findNemo = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 'nemo')
+        console.log('Found NEMO!');
+    }
+}
+
+findNemo(nemo)
---
+++ Sep4/code.md
@@ -0,0 +1,5 @@
+# What is good code?
+
+1.  Scalable
+
+2.  Readability
