707de2f46ba8b4dcb724c149c4c24fde7bd93515
Jordy <jordycorporan95@gmail.com>
2023-09-06 20:40:39 -0400

    Add Example

---
+++ Sep5/unique_pairs.js
@@ -0,0 +1,11 @@
+// Write out an algorithm that takes in an array of numbers and finds if there dsis a unique pair that exist in the list. which equals to a determined sum
+// should also show if there is a pair at all.
+// example [1,2,4,4] = yes and it would be 4,4 that equals 8
+// example [1,2,4] = sum = 8
+
+const findPair = (array) => {
+
+
+}
+
+console.log([1,2,4,4])
