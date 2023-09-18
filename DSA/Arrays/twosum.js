f7558c6f287e382d3b8ac31955a1831be7bcee02
Jordy <jordycorporan95@gmail.com>
2023-09-17 23:14:31 -0400

    better time and space

--- DSA/Arrays/twosum.js
+++ DSA/Arrays/twosum.js
@@ -11,6 +11,15 @@ const twoSum = (nums, target) => {
 
 console.log(twoSum([2,7,11,15],9))
 
-const twosum2 = (nums, target) => {
-    
+const twoSum2 = function (nums, target) {
+    const map = {}
+    for (let i = 0; i < nums.length - 1; i++){
+        const diff = target - nums[i]
+
+        if (diff in map) return [i, map[diff]]
+
+        map[nums[i]] = i
+    }
 }
+
+console.log(twoSum2([2,7,11,15],9))
