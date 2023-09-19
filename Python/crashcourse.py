f1bb694d5c4e9103e94c303f265830b4d4998f82
Jordy <jordycorporan95@gmail.com>
2023-09-18 21:49:20 -0400

    Using basic methods in python

--- Python/crashcourse.py
+++ Python/crashcourse.py
@@ -5,5 +5,29 @@
 # String
 # Bool
 
-print("hello world", False, "potatoe")
-print(4.5, "hello")
+# print("hello world", False, "potatoe")
+# print(4.5, "hello")
+
+
+#? Asking user inputs
+
+# name = input('Name: ')
+# age = input('Age: ')
+# print('Hello', name, 'you are', age, 'years old')
+
+#? Arithmitic operators
+
+# x = 9
+# y = 3
+# result = x * y
+# result = x / y
+# print(result)
+
+#? converting string to other data types
+
+# num = input('Number: ')
+# print(int(num) - 5)
+
+#? Using methods
+hello = 'hello'.upper()
+print(hello)
