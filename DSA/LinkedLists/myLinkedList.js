600923b55755cb5b74f923af1c2bd7ff80c81a5d
Jordy <jordycorporan95@gmail.com>
2023-09-20 23:02:35 -0400

    add prepend

--- DSA/LinkedLists/myLinkedList.js
+++ DSA/LinkedLists/myLinkedList.js
@@ -21,9 +21,23 @@ class LinkedList {
         this.tail = this.head;
         this.length = 1;
     }
+    append(value){
+        const newNode = {
+            value: value,
+            next: null
+        };
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return this
+    }
+    prepend(value){
+        
+    }
 
 }
 
 const myLinkedList = new LinkedList(10)
-
+console.log(myLinkedList.append(5))
+console.log(myLinkedList.append(16))
 console.log(myLinkedList)
