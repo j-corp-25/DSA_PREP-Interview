46e65c6b353277b30416b86bbb319e008fa961da
Jordy <jordycorporan95@gmail.com>
2023-09-20 23:37:03 -0400

    full linklist with node class

--- DSA/LinkedLists/myLinkedList.js
+++ DSA/LinkedLists/myLinkedList.js
@@ -11,7 +11,12 @@
 //     }
 // }
 
-
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null
+    }
+}
 class LinkedList {
     constructor(value){
         this.head = {
@@ -22,20 +27,14 @@ class LinkedList {
         this.length = 1;
     }
     append(value){
-        const newNode = {
-            value: value,
-            next: null
-        };
+        const newNode = new Node(value)
         this.tail.next = newNode;
         this.tail = newNode;
         this.length++;
         return this
     }
     prepend(value){
-        const newNode = {
-            value: value,
-            next: null
-        };
+        const newNode = new Node(value)
         newNode.next = this.head;
         this.head = newNode
         this.length++
