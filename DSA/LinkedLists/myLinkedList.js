74e74f41da3229c8ec91384c7e19462cfc93dde6
Jordy <jordycorporan95@gmail.com>
2023-09-22 13:56:46 -0400

    add linked list

--- DSA/LinkedLists/myLinkedList.js
+++ DSA/LinkedLists/myLinkedList.js
@@ -41,6 +41,30 @@ class LinkedList {
         return this
 
     }
+    printlist() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next
+        }
+        return array
+    }
+    insert(index, value){
+        const newNode = new Node(value)
+        for (let i = 0; i < this.length; i++){
+            if(this[i] === index){
+                this.prepend(newNode)
+                this.head = newNode
+                this.length++
+            }
+
+
+
+        }
+
+
+    }
 
 }
 
@@ -48,5 +72,7 @@ const myLinkedList = new LinkedList(10)
 console.log(myLinkedList.append(5))
 console.log(myLinkedList.append(16))
 console.log(myLinkedList.prepend(100))
+console.log(myLinkedList.insert(2,99))
+console.log(myLinkedList.printlist())
 
 console.log(myLinkedList)
