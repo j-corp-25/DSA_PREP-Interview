a43847e28b6110d68783cbcac1def180a1f61425
Jordy <jordycorporan95@gmail.com>
2023-09-26 22:10:25 -0400

    Stack implemented with arrays

--- DSA/Stack/myStack.js
+++ DSA/Stack/myStack.js
@@ -1,5 +1,59 @@
 
 //LINK LIST IMPLEMENTATION OF STACKS
+// class Node {
+//     constructor(value){
+//         this.value = value,
+//         this.next = null
+//     }
+// }
+
+// class Stack {
+//     constructor() {
+//         this.top = null;
+//         this.bottom = null;
+//         this.length = 0;
+//     }
+//     peek(){
+//         return this.top
+//     }
+//     push(value){
+//         const newNode = new Node(value)
+//         if(this.length === 0){
+//             this.top = newNode
+//             this.bottom = newNode
+//         }else {
+//             const holdingPointer = this.top;
+//             this.top = newNode
+//             this.top.next = holdingPointer
+//         }
+//         this.length++
+//         return this;
+
+//     }
+//     pop(){
+//         if(!this.top){
+//             return null;
+//         }
+//         if(this.top === this.bottom){
+//             this.bottom = null;
+//         }
+//         const holdingPointer = this.top
+//         this.top = this.top.next;
+//         this.length--;
+//         return this;
+//     }
+// }
+
+// const myStack = new Stack()
+// console.log(myStack.push('google'))
+// console.log(myStack.push('udemy'))
+// console.log(myStack.push('youtube'))
+// console.log(myStack.peek())
+// console.log(myStack.pop())
+// console.log(myStack.pop())
+
+//Using ARRAYS
+
 class Node {
     constructor(value){
         this.value = value,
@@ -9,46 +63,23 @@ class Node {
 
 class Stack {
     constructor() {
-        this.top = null;
-        this.bottom = null;
-        this.length = 0;
+   this.array = []
     }
     peek(){
-        return this.top
+        return this.array[this.array.length -1]
     }
     push(value){
-        const newNode = new Node(value)
-        if(this.length === 0){
-            this.top = newNode
-            this.bottom = newNode
-        }else {
-            const holdingPointer = this.top;
-            this.top = newNode
-            this.top.next = holdingPointer
-        }
-        this.length++
+        this.array.push(value)
         return this;
 
     }
     pop(){
-        if(!this.top){
-            return null;
-        }
-        if(this.top === this.bottom){
-            this.bottom = null;
-        }
-        const holdingPointer = this.top
-        this.top = this.top.next;
-        this.length--;
+       this.array.pop();
         return this;
     }
 }
-
-const myStack = new Stack()
-console.log(myStack.push('google'))
-console.log(myStack.push('udemy'))
-console.log(myStack.push('youtube'))
+const myStack = new Stack();
 console.log(myStack.peek())
-console.log(myStack.pop())
-console.log(myStack.pop())
+console.log(myStack.push(5))
+console.log(myStack.peek())
 console.log(myStack.pop())
