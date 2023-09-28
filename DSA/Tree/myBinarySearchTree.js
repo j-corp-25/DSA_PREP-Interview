1f1279ed23b8a8e65d79e2474becec1fefec2c68
Jordy <jordycorporan95@gmail.com>
2023-09-27 22:43:24 -0400

    Create tree skeleton

---
+++ DSA/Tree/myBinarySearchTree.js
@@ -0,0 +1,20 @@
+class Node {
+    constructor(value){
+        this.left = null;
+        this.right = null;
+        this.value = value;
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+
+    }
+    lookup(value){
+
+    }
+}
+const tree = BinarySearchTree()
---
+++ DSA/Tree/notes.md
@@ -0,0 +1,11 @@
+# Binary Search Trees
+
+PROS & Cons
+
++ Better than O(0)
++ Ordered
++ Flexible Size
+
+If it becomes an un-balance then it become linear O(n). One con is that it doesnt have O(1) operations.
+
+Binary Search Trees run at O(log n)
