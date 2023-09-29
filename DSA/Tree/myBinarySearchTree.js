225456b6a005ebca2f7103dcb0df9c3c2ae05e6b
Jordy <jordycorporan95@gmail.com>
2023-09-28 22:41:05 -0400

    Add new ds

--- DSA/Tree/myBinarySearchTree.js
+++ DSA/Tree/myBinarySearchTree.js
@@ -11,10 +11,66 @@ class BinarySearchTree {
         this.root = null;
     }
     insert(value){
+        const newNode = new Node(value)
+        if(!this.root){
+            this.root = newNode
+        }else {
+            let currentNode = this.root;
+            while(true){
+                if(value < currentNode.value) {
+                    if(!currentNode.left){
+                        currentNode.left = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.left
+                }else {
+                    if(!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+        }
+        // if(this.root < value) {
+        //     this.left = newNode
 
+        // }else {
+        //     this.right = newNode
+        // }
+        // this.length++
+        // return this;
     }
     lookup(value){
-
+        if(!this.root){
+            return false;
+        }
+        let currentNode = this.root
+        while(currentNode){
+            if(value < currentNode.value) {
+                currentNode = currentNode.left
+            }else if(value > currentNode.value){
+                currentNode = currentNode.right;
+            }else if (currentNode.value === value) {
+                return currentNode
+            }
+        }
+        return false
     }
 }
-const tree = BinarySearchTree()
+const tree = new BinarySearchTree()
+
+console.log(tree.insert(9))
+console.log(tree.insert(12))
+console.log(tree.insert(8))
+console.log(tree.insert(40))
+console.log(tree.insert(20))
+console.log(tree.insert(5))
+console.log(tree.insert(11))
+
+console.log(tree.lookup(12))
+
+
+// console.log(tree.insert(12))
+// console.log(tree.insert(5))
+// console.log(tree)
