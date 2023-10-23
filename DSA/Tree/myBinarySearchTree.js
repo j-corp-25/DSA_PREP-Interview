96f78823acc10916ede1c08a753c497993304460
Jordy <jordycorporan95@gmail.com>
2023-10-23 13:58:33 -0400

    explore dfs

--- DSA/Tree/myBinarySearchTree.js
+++ DSA/Tree/myBinarySearchTree.js
@@ -1,85 +1,114 @@
 class Node {
-    constructor(value){
-        this.left = null;
-        this.right = null;
-        this.value = value;
-    }
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
 }
 
 class BinarySearchTree {
-    constructor(){
-        this.root = null;
-    }
-    insert(value){
-        const newNode = new Node(value)
-        if(!this.root){
-            this.root = newNode
-        }else {
-            let currentNode = this.root;
-            while(true){
-                if(value < currentNode.value) {
-                    if(!currentNode.left){
-                        currentNode.left = newNode;
-                        return this;
-                    }
-                    currentNode = currentNode.left
-                }else {
-                    if(!currentNode.right) {
-                        currentNode.right = newNode;
-                        return this;
-                    }
-                    currentNode = currentNode.right;
-                }
-            }
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
         }
-        // if(this.root < value) {
-        //     this.left = newNode
+      }
+    }
+    // if(this.root < value) {
+    //     this.left = newNode
 
-        // }else {
-        //     this.right = newNode
-        // }
-        // this.length++
-        // return this;
+    // }else {
+    //     this.right = newNode
+    // }
+    // this.length++
+    // return this;
+  }
+  lookup(value) {
+    if (!this.root) {
+      return false;
     }
-    lookup(value){
-        if(!this.root){
-            return false;
-        }
-        let currentNode = this.root
-        while(currentNode){
-            if(value < currentNode.value) {
-                currentNode = currentNode.left
-            }else if(value > currentNode.value){
-                currentNode = currentNode.right;
-            }else if (currentNode.value === value) {
-                return currentNode
-            }
-        }
-        return false
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode;
+      }
     }
-    breadthFirstSearch
+    return false;
+  }
+  breadthFirstSearch() {
+    let currentNode = this.root;
+    let list = [];
+    let queue = [];
+    queue.push(currentNode);
+
+    while (queue.length > 0) {
+      currentNode = queue.shift();
+      console.log(currentNode.value);
+      list.push(currentNode.value);
+      if (currentNode.left) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right) {
+        queue.push(currentNode.right);
+      }
+    }
+    return list;
+  }
+  breadthFirstSearchR(queue, list) {
+    if (!queue.length) {
+      return list;
+    }
+    let currentNode = queue.shift();
+    list.push(currentNode.value)
+    if (currentNode.left) {
+      queue.push(currentNode.left);
+    }
+    if (currentNode.right) {
+      queue.push(currentNode.right);
+    }
+    return this.breadthFirstSearchR(queue,list)
+  }
 }
-const tree = new BinarySearchTree()
+const tree = new BinarySearchTree();
 
-console.log(tree.insert(9))
-console.log(tree.insert(12))
-console.log(tree.insert(8))
-console.log(tree.insert(40))
-console.log(tree.insert(20))
-console.log(tree.insert(5))
-console.log(tree.insert(11))
-
-console.log(tree.lookup(12))
-
+console.log(tree.insert(9));
+console.log(tree.insert(12));
+console.log(tree.insert(8));
+console.log(tree.insert(40));
+console.log(tree.insert(20));
+console.log(tree.insert(5));
+console.log(tree.insert(11));
+console.log(tree.breadthFirstSearch())
+console.log(tree.breadthFirstSearchR([tree.root], []));
 
 const traverse = (node) => {
-    const tree = {value: node.value };
-    tree.left = node.left === null ? null :
-    traverse(node.left);
-    tree.right = node.right === null ? null :
-    traverse(node.right)
-    return tree;
-}
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+};
 // console.log(tree.insert(12))
 // console.log(tree.insert(5))
 // console.log(tree)
